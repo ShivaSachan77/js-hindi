3254e255b4715b52db3e3cd92b387eff756dc897
ShivaSachan77 <140420817+ShivaSachan77@users.noreply.github.com>
2024-10-15 16:23:11 +0000

    datatype summary

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+
+
+// console.log(2 > "4");
+// console.log("2" > 1);
+
+//avoid these all comparision below
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+// console.log("2" === 2);//will also check datatype and value
+
+
---
+++ datatypes-summary.js
@@ -0,0 +1,38 @@
+//Primitives
+//7 types: Number, String, null, undefined(variable is declared but value is not assigned), symbol(to make variable unique), Boolean, BigInt
+// const score = 100
+// console.log(score);
+
+// const logged = false
+// const temp = null
+// let userEmail;
+// console.table([logged, temp, userEmail])
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+// const bigNumber = 9989898989898989899999n
+// console.log(bigNumber);
+// console.log(typeof bigNumber);
+
+
+
+// Refernce type: (nonPrimitive)
+
+//Array, Objects, functions
+// const heros = ["Shikha", 90, "Shiktiman"]
+// let obj = {name: "Shiva",
+//     age: 23,
+//     RollNo: 222
+// }
+// console.log(obj);
+// console.log(heros);
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+console.log(typeof myFunction);
+
+
