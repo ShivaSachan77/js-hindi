d184ac2dba257e345f1c0eeb4ad52f050e880622
ShivaSachan77 <140420817+ShivaSachan77@users.noreply.github.com>
2024-10-15 18:16:55 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,46 @@
+// const name = "Shiva "
+// const countRepo = 50
+
+// console.log(name + countRepo + " Value");
+
+// console.log(`hello my name is ${name} repo count is ${countRepo}`);
+
+//behind the scene decleration of string is
+// const gameName = new String("tufSHADOW")
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('A'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);//substring: will give letters form 0 till 4-1, it excludes last   index
+
+// const anotherString = gameName.slice(-8, 4)//can use negative values with slice
+// console.log(anotherString);
+
+// const newStr1 = "     Shiva       "
+// console.log(newStr1.trim());//trim(): removes spaces from our string
+
+//replace(): replaces specified character with provided character
+// const url = "https://shiva.com/shiva%20sachan"
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('shiva'));
+// console.log(url.includes('diksha'));
+
+//split(): seperator, limit
+const str2 = "sonu-ke-titu- ki-sweety"
+console.log(str2.split("-"));
+
+
+
+
+
+
+
+
+
+
