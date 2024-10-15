a8c427a2dcde5f91e3578a43d98f77b9ccdd43d1
ShivaSachan77 <140420817+ShivaSachan77@users.noreply.github.com>
2024-10-15 10:57:47 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = 33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// score = "33"
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// score = "33abc" 
+// valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//NaN is not a number
+//33=>33
+//"33abc"=>NaN
+
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+// let isLoggedIn = "Shiva"
+// let isLoggedIn = null
+// let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNum = String(someNumber)
+console.log(stringNum);
+console.log(typeof stringNum);
+
+
