33397e20d64835e4fcccec3f95db645379c6ed9d
ShivaSachan77 <140420817+ShivaSachan77@users.noreply.github.com>
2024-10-15 15:07:57 +0000

    code config 3

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,9 +24,43 @@
 // let booleanIsLoggedIn =Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let someNumber = 33
-let stringNum = String(someNumber)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let someNumber = 33
+// let stringNum = String(someNumber)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+//  ********************************     Operation     ********************************************************
+// let value = 3
+// let negVal = -value
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2**3);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " gym"
+// let str3 = str1+str2
+// console.log(str3);
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log(1+2+"3");
+// console.log("1"+2+2);
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);//error
+
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
+
+
 
 
