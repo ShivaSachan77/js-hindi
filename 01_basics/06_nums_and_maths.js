326eedca9cbf229e624a752bbedf6944d78e4c79
ShivaSachan77 <140420817+ShivaSachan77@users.noreply.github.com>
2024-10-17 10:47:58 +0000

    maths and nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,41 @@
+const score = 400
+const num = new Number(1000)
+
+
+// console.log(score);
+// console.log(num);
+// console.log(num.toString.length);//toString avils all funcs of string in number
+// console.log(num.toFixed(2));//toFixed(): defines precision of a value
+
+const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3));
+// const hunds = 1000000
+// console.log(hunds.toLocaleString());//automatically applies commas between digits according to us standards
+// console.log(hunds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++
+//maths library
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(4.564));
+// console.log(Math.ceil(4.64));
+// console.log(Math.floor(4.64));
+// console.log(Math.min(4 , 3,4,5,6));
+// console.log(Math.max(4.64,3,4,5,6));
+
+
+
+// console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 20
+// Math.floor(Math.random() * (max - min + 1)) + min
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
