ee2cc34f736ad43a85eb1e69483fab619068f393
ShivaSachan77 <140420817+ShivaSachan77@users.noreply.github.com>
2024-10-17 17:22:38 +0000

    arrays01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,24 @@
+//arrays 
+const myArr = [1,2,3,4,5]
+//arrays in js are resizable
+//has zero base indexing
+//shallow copies: is a copy who share the same reference as thos of the source object from which the copy was made
+//deep copy: is a copy of an object whose reference do not share the same reference
+
+const arr = new Array(1,2,33,4)
+// console.log(arr[3]);
+
+//Arrays methods
+arr.push(6)
+arr.push(8)
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+// arr.unshift(0)//will insert at the starting of array
+// console.log(arr);
+// arr.shift()//willremove from the starting of array
+// console.log(arr);
+
+
+
+
